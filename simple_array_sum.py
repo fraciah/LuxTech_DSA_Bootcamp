01008ea5518383d32929d1e43713a563f96a8b25
fraciah <karfraciah@gmail.com>
2022-06-18 15:36:54 +0300

    week one assignment

--- simple_array_sum.py
+++ simple_array_sum.py
@@ -1,14 +1,14 @@
-# import numpy as np
-# arr = np.array([1,2,3,4,10,11])
-
-# def simpleArraySum(ar):
-#     return ar.sum()
-# print(simpleArraySum(arr))
-
 def simpleArraySum(ar):
     ar_sum = 0
     for i in ar:
         ar_sum = ar_sum + i
     return ar_sum
-    
-print(simpleArraySum(ar=[1,2,3,4,10,11]))
+arr=[1,2,3,4,10,11]
+print(simpleArraySum(arr))
+
+# import numpy as np
+# arr = np.array([1,2,3,4,10,11])
+
+# def simpleArraySum2(ar):
+#     return ar.sum()
+# print(simpleArraySum2(arr))
